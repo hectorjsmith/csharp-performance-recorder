245a2e1c34060971ca93e0c4a9053531f25abc53
Hector <dev@hsmith.org>
2019-10-17 08:32:38 +0100

    task #2: Add test to ensure actions still performed in recorder
    - Add unit test to ensure that actions are still executed when they are run
      in the performance recorder object

---
+++ PerformanceRecorderTest/Recorder/PerformanceRecorderTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using PerformanceRecorder.Recorder;
+using PerformanceRecorder.Recorder.Impl;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PerformanceRecorderTest.Recorder
+{
+    class PerformanceRecorderTest
+    {
+        [Test]
+        public void TestGivenActivePerformanceRecorderWhenActionExecutionTimeRecordedThenActionIsExecuted()
+        {
+            IPerformanceRecorder recorder = new ActivePerformanceRecorderImpl();
+            HelperFunctionToEnsureActionsExectutedInRecorders(recorder);
+        }
+
+        [Test]
+        public void TestGivenInactivePerformanceRecorderWhenActionExecutionTimeRecordedThenActionIsExecuted()
+        {
+            IPerformanceRecorder recorder = new InactivePerformanceRecorderImpl();
+            HelperFunctionToEnsureActionsExectutedInRecorders(recorder);
+        }
+
+        private void HelperFunctionToEnsureActionsExectutedInRecorders(IPerformanceRecorder recorder)
+        {
+            int initialValue = 10;
+            int value = initialValue;
+            recorder.RecordExecutionTime("test", () => value *= value);
+
+            Assert.AreNotEqual(initialValue, value, "Value should have been modified in the performance recorder");
+        }
+    }
+}
