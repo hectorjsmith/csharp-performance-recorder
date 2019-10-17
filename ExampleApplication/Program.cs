393be8ad3f78aa886aefc68ee712d10811144c12
Hector <dev@hsmith.org>
2019-10-17 16:10:21 +0100

    task #3: Add new project to solution
    - Add the new example project to the solution and add reference to recorder
      project

---
+++ ExampleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
