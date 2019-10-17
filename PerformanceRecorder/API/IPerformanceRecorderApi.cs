fac205e01a8510a64d9d51b94dbf5e085da356c7
Hector <dev@hsmith.org>
2019-10-17 14:44:23 +0100

    task #5: Add new API class
    - Add a new API interface and class to expose certain parts of the library
      to external projects

---
+++ PerformanceRecorder/API/IPerformanceRecorderApi.cs
@@ -0,0 +1,16 @@
+﻿using PerformanceRecorder.Result;
+using System.Collections.Generic;
+
+namespace PerformanceRecorder.API
+{
+    public interface IPerformanceRecorderApi
+    {
+        void EnablePerformanceRecording();
+
+        void DisablePerformanceRecording();
+
+        void ResetRecorder();
+
+        ICollection<IRecordingResult> GetResults();
+    }
+}
---
+++ PerformanceRecorder/API/Impl/PerformanceRecorderApiImpl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PerformanceRecorder.Result;
+
+namespace PerformanceRecorder.API.Impl
+{
+    public class PerformanceRecorderApiImpl : IPerformanceRecorderApi
+    {
+        public void DisablePerformanceRecording()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EnablePerformanceRecording()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<IRecordingResult> GetResults()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetRecorder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
