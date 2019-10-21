399e2798c79daa05c7acfdf1e0f37575a532e63f
Hector <dev@hsmith.org>
2019-10-21 17:25:10 +0100

    task #16: Add new test to check precision
    - Add a new test to ensure performance recording has sub-millisecond
      precision by running a method that takes an average of 0 to 1ms to execute a
      large number of times and asserting that the average time is not 0.0

--- PerformanceRecorderTest/Recorder/PerformanceRecorderTest.cs
+++ PerformanceRecorderTest/Recorder/PerformanceRecorderTest.cs
@@ -1,7 +1,12 @@
 ﻿using NUnit.Framework;
+using PerformanceRecorder.Attribute;
+using PerformanceRecorder.Manager;
 using PerformanceRecorder.Recorder;
 using PerformanceRecorder.Recorder.Impl;
+using PerformanceRecorder.Result;
 using PerformanceRecorder.Result.Impl;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PerformanceRecorderTest.Recorder
 {
@@ -28,6 +33,36 @@ namespace PerformanceRecorderTest.Recorder
             recorder.RecordExecutionTime(new MethodDefinitionImpl("t", "t", "t"), () => value *= value);
 
             Assert.AreNotEqual(initialValue, value, "Value should have been modified in the performance recorder");
+        }
+
+        [Test]
+        public void TestGivenActivePerformanceRecorderWhenShortMethodRecordedThenNoPrecisionLost()
+        {
+            int runCount = 10_000;
+
+            StaticRecorderManager.IsRecordingEnabled = true;
+            for (int i = 0; i < runCount; i++)
+            {
+                HelperFunctionToRecord(i);
+            }
+
+            ICollection<IRecordingResult> results = StaticRecorderManager.GetRecorder().GetResults();
+            Assert.AreEqual(1, results.Count, "Only one result was expected");
+
+            IRecordingResult firstResult = results.First();
+            Assert.AreNotEqual(0.0, firstResult.Sum, "Sum of all executions should not be 0.0");
+            Assert.AreNotEqual(0.0, firstResult.Avg, "Average of all executions should not be 0.0");
+        }
+
+        [PerformanceLogging]
+        private void HelperFunctionToRecord(int i)
+        {
+            if (i % 10 == 0)
+            {
+                // One in ten times, wait 1ms
+                // This is to ensure this method takes on average, between 0 and 1ms
+                System.Threading.Thread.Sleep(1);
+            }
         }
     }
 }
