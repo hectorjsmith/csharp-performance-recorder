0bb7ecd3d4b987b38c8dfe2a6213941162a4601c
Hector <dev@hsmith.org>
2019-10-22 07:13:38 +0100

    task #6: Implement formatter for formatted string output
    - Implement the code to generate a formatted string based on a collection
      of results
    - Add test to ensure the output string matches the expected format and all
      data is padded correctly

--- PerformanceRecorder/Result/Formatter/Impl/FormattedStringResultFormatterImpl.cs
+++ PerformanceRecorder/Result/Formatter/Impl/FormattedStringResultFormatterImpl.cs
@@ -1,14 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace PerformanceRecorder.Result.Formatter.Impl
 {
     class FormattedStringResultFormatterImpl : IResultFormatter<string>
     {
+        private const string RawFormatString = "{0,_key_len_}  count: {1,_count_len_}  sum: {2,_num_len_:0.00}  avg: {3,_num_len_:0.00}  max: {4,_num_len_:0.00}  min: {5,_num_len_:0.00}";
+
         public string FormatAs(ICollection<IRecordingResult> results)
         {
-            throw new NotImplementedException();
+            if (results.Count == 0)
+            {
+                return "";
+            }
+
+            int keyLength = FindLengthOfLongestResultName(results);
+            int countLength = FindLengthOfLongestCount(results);
+            int numLength = FindLengthOfLongestValue(results);
+
+            string formatString = RawFormatString
+                .Replace("_key_len_", "" + keyLength)
+                .Replace("_count_len_", "" + countLength)
+                .Replace("_num_len_", "" + numLength);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (IRecordingResult result in results.OrderByDescending(r => r.Sum))
+            {
+                string resultName = string.Format("{0}.{1}.{2}", result.Namespace, result.ClassName, result.MethodName);
+
+                sb.Append(string.Format(formatString,
+                    resultName, result.Count, result.Sum, result.Avg, result.Max, result.Min));
+                sb.Append(Environment.NewLine);
+            }
+            return sb.ToString();
         }
+
+        private int FindLengthOfLongestResultName(ICollection<IRecordingResult> results)
+        {
+            return results.Select(r => string.Format("{0}.{1}.{2}", r.Namespace, r.ClassName, r.MethodName).Length).Max();
+        }
+
+        private int FindLengthOfLongestCount(ICollection<IRecordingResult> results)
+        {
+            double maxSum = results.Select(r => r.Count).Max();
+            return string.Format("{0:0}", maxSum).Length;
+        }
+
+        private int FindLengthOfLongestValue(ICollection<IRecordingResult> results)
+        {
+            double maxSum = results.Select(r => r.Sum).Max();
+            return string.Format("{0:0.00}", maxSum).Length;
+        }
+
     }
 }
--- PerformanceRecorderTest/Result/Formatter/FormattedStringResultFormatterTest.cs
+++ PerformanceRecorderTest/Result/Formatter/FormattedStringResultFormatterTest.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using NUnit.Framework;
+using PerformanceRecorder.Result;
+using PerformanceRecorder.Result.Formatter;
+using PerformanceRecorder.Result.Formatter.Impl;
+using PerformanceRecorder.Result.Impl;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +11,55 @@ namespace PerformanceRecorderTest.Result.Formatter
 {
     class FormattedStringResultFormatterTest
     {
+        [Test]
+        public void TestGivenResultCollectionWhenFormattedAsPlainStringThenOutputMatchesExpected()
+        {
+            ICollection<IRecordingResult> results = GenerateMockResults();
+            IResultFormatter<string> formatter = new FormattedStringResultFormatterImpl();
+
+            string output = formatter.FormatAs(results);
+            string expectedOutput
+                = "nnnn.cccc.mmmm2  count: 3  sum: 1240.00  avg:  413.33  max: 1020.00  min:   20.00" + Environment.NewLine
+                + "   nnn.ccc.mmm1  count: 3  sum:  620.00  avg:  206.67  max:  510.00  min:   10.00" + Environment.NewLine
+                + "      nn.cc.mm0  count: 3  sum:    0.00  avg:    0.00  max:    0.00  min:    0.00" + Environment.NewLine;
+
+            Assert.Greater(output.Length, 0, "Output string length should be greater than 0");
+            Assert.AreEqual(expectedOutput, output, "Formatted output did not match expected format");
+        }
+
+        [Test]
+        public void TestGivenEmptyResultCollectionWhenFormattedAsPlainStringThenBlankOutputReturned()
+        {
+            ICollection<IRecordingResult> results = new List<IRecordingResult>();
+            IResultFormatter<string> formatter = new FormattedStringResultFormatterImpl();
+
+            string output = formatter.FormatAs(results);
+            Assert.AreEqual(0, output.Length, "Output string length should be zero");
+        }
+
+        private ICollection<IRecordingResult> GenerateMockResults()
+        {
+            ICollection<IRecordingResult> results = new List<IRecordingResult>();
+            for (int i = 0; i < 3; i++)
+            {
+                IRecordingResult result = new RecordingResultImpl(
+                    new MethodDefinitionImpl(RepeatString("n", i + 2), RepeatString("c", i + 2), RepeatString("m", i + 2) + i), i * 100);
+                result.AddResult(i * 10);
+                result.AddResult(i * 510);
+                results.Add(result);
+            }
+
+            return results;
+        }
+
+        private string RepeatString(string input, int count)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                sb.Append(input);
+            }
+            return sb.ToString();
+        }
     }
 }
