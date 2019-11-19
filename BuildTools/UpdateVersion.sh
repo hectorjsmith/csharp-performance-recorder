5170381f6268b83088889eca276b92adb7a55698
Hector <dev@hsmith.org>
2019-11-19 16:03:41 +0000

    task #25: Remove publish branch restriction for testing
    Update the gitlab CI file to remove the branch restrictions to ensure the
    new build process works as expected.
    Add log message to code updating version data to display the new version
    being set for debugging purposes.

--- BuildTools/UpdateVersion.sh
+++ BuildTools/UpdateVersion.sh
@@ -3,4 +3,6 @@
 csprojPath=PerformanceRecorder/PerformanceRecorder.csproj
 version=`git describe --tags`
 
+echo "Updating version to: ${version}"
+
 sed -e "s;<Version>.*</Version>;<Version>${version}</Version>;g" -i ${csprojPath} -b
