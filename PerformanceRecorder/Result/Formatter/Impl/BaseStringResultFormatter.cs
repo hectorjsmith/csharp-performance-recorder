ec7aeb579bf4a6305003f59c8b57401f31827d77
Hector <dev@hsmith.org>
2020-06-18 10:56:38 +0100

    #38: Update formatter to handle empty results
    Add a check at the start of the nested result formatter to ensure the code
    can handle an empty result collection by returning an empty string.
    Update code finding the length of the longest value and count to handle
    empty collections by returning 0 by default.

--- PerformanceRecorder/Result/Formatter/Impl/BaseStringResultFormatter.cs
+++ PerformanceRecorder/Result/Formatter/Impl/BaseStringResultFormatter.cs
@@ -67,13 +67,13 @@ namespace PerformanceRecorder.Result.Formatter.Impl
 
         protected int FindLengthOfLongestCount(ICollection<IRecordingResult> results)
         {
-            double maxSum = results.Select(r => r.Count).Max();
+            double maxSum = results.Select(r => r.Count).DefaultIfEmpty(0).Max();
             return string.Format("{0:0}", maxSum).Length;
         }
 
         protected int FindLengthOfLongestValue(ICollection<IRecordingResult> results)
         {
-            double maxSum = results.Select(r => r.Sum).Max();
+            double maxSum = results.Select(r => r.Sum).DefaultIfEmpty(0).Max();
             string sumFormat = ReplaceDecimalPlacePaddingInString("{0:0" + DecimalPointPlaceholder + "}");
 
             return string.Format(sumFormat, maxSum).Length;
